3d6b5c294cc4de63a172ab63c3f300d09b22410e
Bowen Zheng <bzheng2438@gmail.com>
2025-11-02 11:00:43 -0800

    Delete user_interface directory

--- user_interface/main.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-if __name__ == "__main__":
-    # Runs the nanopositioner_gui.py script as an independent process
-    subprocess.Popen([sys.executable, "nanopositioner_gui.py"])
-    # Runs the camera.py script as an independent process
-    subprocess.Popen([sys.executable, "camera.py"])
